56f79a812b694219ede15318e9ae3f9e930cf68c
Daniel O'Flynn <danielpoflynn@gmail.com>
2024-05-09 18:42:59 +0100

    changes directories

--- custom_shell_cmd.sh
+++ custom_shell_cmd.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-gogo_read_lines() {
-    saved_commands=()
+# Reads in the directory shortcuts
+gogo_read_shortcuts() {
+    shortcuts=() # shortcuts saved in structure ( name0 directory0 name1 directory1 etc )
     INFILE=~/custom_shell_cmds/directory_shortcuts.txt
-    while read -r LINE
-        do
-        printf '%s\n' "$LINE"
+    while read -ra LINE; do
+        # echo ${LINE[1]}
+        shortcuts+=( "${LINE[@]}" ) 
     done < "$INFILE"
+    # for i in "${shortcuts[@]}"; do
+    # echo "$i"
+    # done
+}
+
+# Writes the directory shortcuts to a file
+gogo_write_lines() {
+    echo "hello"
 }
 
 # A hello world bash function
 gogo () {
-
     if [[ "$1" == "add" ]]; then
     # TODO add adding function
         echo "Im in $(pwd)"
@@ -19,10 +27,19 @@ gogo () {
     # TODO add updating function
         echo "updating"
     else 
-        # Read in
-        echo "ga"
+        i=0
+        while [ "$i" -lt "${#shortcuts[@]}" ]; do
+            if [[ "${shortcuts[$i]}" == "$1" ]]; then
+                echo "Changing directories to ${shortcuts[$i + 1]}"
+                cd 
+                cd ${shortcuts[$i + 1]}
+                break
+            fi
+
+            i+=2
+        done
     fi
 
 }
 
-gogo_read_lines
+gogo_read_shortcuts

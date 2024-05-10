3c2a5275465743dcab5c1ec1301402f5f998383d
Daniel O'Flynn <danielpoflynn@gmail.com>
2024-05-10 12:00:56 +0100

    add directories

--- custom_shell_cmd.sh
+++ custom_shell_cmd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO instead of reading in as global variables, read in every time to a local variable
 # Reads in the directory shortcuts
 gogo_read_shortcuts() {
     shortcuts=() # shortcuts saved in structure ( name0 directory0 name1 directory1 etc )
@@ -13,19 +14,43 @@ gogo_read_shortcuts() {
     # done
 }
 
-# Writes the directory shortcuts to a file
-gogo_write_lines() {
-    echo "hello"
+# Writes the directory shortcuts to shortcuts file
+gogo_write_shortcuts() {
+    echo "${shortcuts[@]}" > ~/custom_shell_cmds/directory_shortcuts.txt
 }
 
-# A hello world bash function
+
 gogo () {
+    
+    protected_vars=( "add" "update" "remove") # These are special keywords that cannot be used as nicknames
+
     if [[ "$1" == "add" ]]; then
-    # TODO add adding function
-        echo "Im in $(pwd)"
+    # Check that the string isn't empty
+        if [ -z "$2" ]; then 
+            echo "gogo add error: nickname cannot be empty"
+            return 1
+        fi
+        
+        # Ensure it's not a protected word
+        if [[ protected_vars =~ [[:space:]]${2}[[:space:]] ]]; then 
+            echo "gogo add error: nickname cannot be empty"
+            return 1
+        fi
+
+
+        shortcuts+=( "$2" )
+        shortcuts+=( "$(pwd)" )
+        # echo "${shortcuts[@]}"
+        gogo_write_shortcuts
+
     elif [[ "$1" == "update" ]]; then
     # TODO add updating function
         echo "updating"
+    elif [[ "$1" == "remove" ]]; then
+    # TODO add removing function
+    echo "none"
+    elif [[ "$1" == "list" ]]; then
+    echo "no"
     else 
         i=0
         while [ "$i" -lt "${#shortcuts[@]}" ]; do
@@ -43,3 +68,4 @@ gogo () {
 }
 
 gogo_read_shortcuts
+gogo add dance
